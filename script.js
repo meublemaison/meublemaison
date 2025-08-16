c3fdeb9536dd1d3e6de6bbacf26db856e21d03ce
meublemaison <chebb4999@gmail.com>
2025-08-16 06:28:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Showroom loaded successfully!");
+
+// If you want custom behaviors, add them here.
+// For example, you could alert visitors after 5 seconds:
+setTimeout(() => {
+  console.log("Thanks for visiting our marble showroom!");
+}, 5000);
